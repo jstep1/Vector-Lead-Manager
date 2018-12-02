b9f2b457b2b1465f4f3c1bdd5bec090d03eeab2b
Jeremy Stepanovich <jstepanovich91@gmail.com>
2018-12-02 16:13:57 -0600

    updated program

---
+++ vlm.js
@@ -0,0 +1,4 @@
+
+
+
+    // $("#nlactivity").append('<button class="btn btn-danger" type="button">NH</button> <button class="btn btn-primary" type="button">LM</button> <button class="btn btn-secondary" type="button">ST</button> <button class="btn btn-success" type="button">Schedule Demo</button>')
